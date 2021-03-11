173d7414e2292110294d723edb7f31b6a97ebbda
Randy Carrero <randycarrero9@gmail.com>
2021-03-11 12:39:55 -0500

    Linked change from dummy to real one.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:gunpla_database/app/app.dart';
 import 'package:gunpla_database/backend/backend.dart';
 
 void main() {
-  final backend = Backend('https://api.spacexdata.com/v4');
+  final backend = Backend('https://raw.githubusercontent.com/randycarrero/gunplajson/main/model.json');
 
   runApp(
     GunplaDatabaseApp(
