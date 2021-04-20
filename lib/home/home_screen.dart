0f363f8dfebb24fae305b8720184736cb3b4f02d
Randy Carrero <randycarrero9@gmail.com>
2021-04-20 13:42:24 -0400

    Removed Bottom Nav Bar
    Navigation for bottom Bar removed temporarily. not needed for now and is dead code at the moment.

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -18,6 +18,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int currentTAb = 0;
   final List<Widget> screens = [HomeScreen(), Series(), Profile(), Setting()];
+  List gunplas = [];
+  List filteredgunplas = [];
+  bool isSearching = false;
 
   final PageStorageBucket bucket = PageStorageBucket();
   Widget currentScreen = HomeScreen();
@@ -25,177 +28,44 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Gunpla Database'),
-        ),
-        body: FutureBuilder(
-          future: context.read<Backend>().getGunplas(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return const Center(
-                child: Text('An error occurred.'),
-              );
-            } else if (!snapshot.hasData) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              final gunplas = snapshot.data;
+      appBar:
+          AppBar(title: !isSearching ? Text('Gunpla Database') : TextField()),
+      body: FutureBuilder(
+        future: context.read<Backend>().getGunplas(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('An error occurred.'),
+            );
+          } else if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            final gunplas = snapshot.data;
 
-              return ListView(
-                children: [
-                  for (final gunpla in gunplas) ...[
-                    GunplaListTile(
-                      gunpla: gunpla,
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return GunplaDetailsScreen(gunpla: gunpla);
-                            },
-                          ),
-                        );
-                      },
-                    ),
-                    const Divider(height: 0.0),
-                  ]
-                ],
-              );
-            }
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.black,
-          child: Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
-          onPressed: () {},
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        bottomNavigationBar: BottomAppBar(
-          shape: CircularNotchedRectangle(),
-          notchMargin: 10,
-          color: Colors.black,
-          child: Container(
-            height: 60,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    MaterialButton(
-                      minWidth: 40,
-                      onPressed: () {
-                        setState(() {
-                          currentScreen = HomeScreen();
-                          currentTAb = 0;
-                        });
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.dashboard,
-                            color: currentTAb == 0 ? Colors.blue : Colors.white,
-                          ),
-                          Text(
-                            'Home',
-                            style: TextStyle(
-                              color:
-                                  currentTAb == 0 ? Colors.blue : Colors.white,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    MaterialButton(
-                      minWidth: 40,
-                      onPressed: () {
-                        setState(() {
-                          currentScreen = Series();
-                          currentTAb = 1;
-                        });
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.tv,
-                            color: currentTAb == 1 ? Colors.blue : Colors.white,
-                          ),
-                          Text(
-                            'Series',
-                            style: TextStyle(
-                              color:
-                                  currentTAb == 1 ? Colors.blue : Colors.white,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                // Right Side Bar Section!
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    MaterialButton(
-                      minWidth: 40,
-                      onPressed: () {
-                        setState(() {
-                          currentScreen = Profile();
-                          currentTAb = 2;
-                        });
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.account_circle,
-                            color: currentTAb == 2 ? Colors.blue : Colors.white,
-                          ),
-                          Text(
-                            'Profile',
-                            style: TextStyle(
-                              color:
-                                  currentTAb == 2 ? Colors.blue : Colors.white,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    MaterialButton(
-                      minWidth: 40,
-                      onPressed: () {
-                        setState(() {
-                          currentScreen = Setting();
-                          currentTAb = 3;
-                        });
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.settings,
-                            color: currentTAb == 3 ? Colors.blue : Colors.white,
-                          ),
-                          Text(
-                            'Settings',
-                            style: TextStyle(
-                              color:
-                                  currentTAb == 3 ? Colors.blue : Colors.white,
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                )
+            return ListView(
+              children: [
+                for (final gunpla in gunplas) ...[
+                  GunplaListTile(
+                    gunpla: gunpla,
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return GunplaDetailsScreen(gunpla: gunpla);
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                  const Divider(height: 0.0),
+                ]
               ],
-            ),
-          ),
-        ));
+            );
+          }
+        },
+      ),
+    );
   }
 }
