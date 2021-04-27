7ad121d4c067946ad37d93d3159054aee2f3dbd6
Randy <randycarrero9@gmail.com>
2021-04-27 18:05:20 -0400

    Error Cleaned

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:gunpla_database/app/theme.dart';
 import 'package:gunpla_database/backend/backend.dart';
 import 'package:gunpla_database/home/home_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:gunpla_database/sign_up/sign_up_screen.dart';
 
 class GunplaDatabaseApp extends StatelessWidget {
@@ -14,13 +15,20 @@ class GunplaDatabaseApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.light(),
-      darkTheme: AppTheme.dark(),
-      home: Provider.value(
-        value: backend,
-        child: HomeScreen(),
+    return Provider.value(
+      value: backend,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light(),
+        darkTheme: AppTheme.dark(),
+        home: StreamBuilder<User>(
+            stream: backend.currentUserStream,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return SignUpScreen();
+              }
+              return HomeScreen();
+            }),
       ),
     );
   }
