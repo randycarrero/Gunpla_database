649818ce06e0b02e454e5a85d7cf7c6e8887bfe2
Randy Carrero <randycarrero9@gmail.com>
2021-05-21 14:27:14 -0400

    Code updated
    Some of the code that was use has been deprecated and needed to be updated.

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:gunpla_database/backend/backend.dart';
 import 'package:provider/provider.dart';
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+
+// ignore: implementation_imports
 import 'package:flutter/src/widgets/framework.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -20,44 +26,65 @@ class _HomeScreenState extends State<HomeScreen> {
             icon: const Icon(Icons.logout),
           ),
           title: const Text('Gunpla Database')),
-      body: Container(),
-
+      body: GunplaListTile(),
     );
   }
 }
 
-class Gunpla_list_tile extends StatefulWidget {
+class GunplaListTile extends StatefulWidget {
   @override
-  _Gunpla_list_tileState createState() => _Gunpla_list_tileState();
+  _GunplaListTileState createState() => _GunplaListTileState();
 }
 
-class _Gunpla_list_tileState extends State<Gunpla_list_tile>{
+class _GunplaListTileState extends State<GunplaListTile> {
+  Future getPost() async {
+    var firestore = FirebaseFirestore.instance;
+    QuerySnapshot qn = await firestore.collection("gunpla").get();
+    return qn.docs;
+  }
+
   @override
-  Widget build(BuildContext content){
-    return Container();
+  Widget build(BuildContext content) {
+    return Container(
+      child: FutureBuilder(
+          future: getPost(),
+          builder: (_, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: Text("Loading...."),
+              );
+            } else {
+              ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (_, index) {
+                    return ListTile(
+                      title: Text(snapshot.data[index].data["name"]),
+                    );
+                  });
+            }
+          }),
+    );
   }
 }
 
 class MyStatelessWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
+
 class GunplaDetailsScreen extends StatefulWidget {
   @override
   _GunplaDetailsScreenState createState() => _GunplaDetailsScreenState();
 }
+
 class _GunplaDetailsScreenState extends State<GunplaDetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
-}//4:33
+}//9:29
 
 //      FutureBuilder(
 //      future: context.read<Backend>().getGunplas(),
