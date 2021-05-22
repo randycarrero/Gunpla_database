52f512cb08d221e5eaec8fefdc9b755ed2ff5892
Randy Carrero <randycarrero9@gmail.com>
2021-05-22 11:29:34 -0400

    removed unused

--- lib/backend/firestore.dart
+++ lib/backend/firestore.dart
@@ -1,13 +1,4 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:meta/meta.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:gunpla_database/backend/backend.dart';
-import 'package:gunpla_database/gunpla_details/gunpla_details_screen.dart';
-import 'package:gunpla_database/home/gunpla_list_tile.dart';
-import 'package:provider/provider.dart';
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 final CollectionReference _mainCollection = _firestore.collection('gunpla');
