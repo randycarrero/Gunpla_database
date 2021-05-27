a7e6f95bd524092e72f48c69de64e585d5ae99e7
Randy Carrero <randycarrero9@gmail.com>
2021-05-27 19:33:22 -0400

    qingshui Help with rebuilding code.

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+class LeadingImage extends StatelessWidget {
+  final String url;
+
+  const LeadingImage(this.url);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(200),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(400),
+        child: Image.network(url),
+      ),
+    );
+  }
+}
+
 class HomeScreen extends StatelessWidget {
   final CollectionReference collectionReference =
       FirebaseFirestore.instance.collection('gunpla');
@@ -14,69 +33,35 @@ class HomeScreen extends StatelessWidget {
             icon: const Icon(Icons.logout),
           ),
           title: const Text('Gunpla Database')),
-      body: Container(
-        child: Column(
-          children: [
-            Expanded(
-                child: StreamBuilder(
-              stream: collectionReference.snapshots(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasData) {
-                  return ListView(
-                    children: snapshot.data.docs
-                        .map((e) => Column(
-                              children: [
-                                ListTile(
-                                    isThreeLine: true,
-                                    leading:
-                                        // CircleAvatar(
-                                        //   backgroundImage:
-                                        //       NetworkImage(userAvatarUrl),
-                                        // ),
-                                        //   CircleAvatar(
-                                        // backgroundColor: Colors.brown.shade800,
-                                        // child: const Text('AH'),
-                                        Material(
-                                      clipBehavior: Clip.antiAlias,
-                                      borderRadius: BorderRadius.circular(8.0),
-                                      child: AspectRatio(
-                                          aspectRatio: 3 / 2,
-                                          child: Image.network(
-                                              snapshot.data.documents[index]
-                                                  .data["url"],
-                                              fit: BoxFit.cover)),
-                                    ),
-                                    title: Row(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          Expanded(
-                                            child: Text(e['name']),
-                                          )
-                                        ]),
-                                    subtitle: Row(
-                                      children: [
-                                        Text(
-                                          e['series'],
-                                          maxLines: 2,
-                                          overflow: TextOverflow.ellipsis,
-                                        )
-                                      ],
-                                    )),
-                                const Divider(height: 0.0),
-                              ],
-                            ))
-                        .toList(),
+      body: SingleChildScrollView(
+          child: StreamBuilder(
+        stream: collectionReference.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            var docs = snapshot.data.docs;
+            return ListView.builder(
+                primary: false,
+                shrinkWrap: true,
+                itemCount: docs.length,
+                itemBuilder: (_, index) {
+                  var document = docs[index];
+                  return ListTile(
+                    isThreeLine: true,
+                    leading: LeadingImage(document['image']),
+                    title: Text(document['name']),
+                    subtitle: Text(
+                      document['series'],
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   );
-                }
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
-            ))
-          ],
-        ),
-      ),
+                });
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      )),
     );
   }
 }
