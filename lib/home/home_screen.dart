4f63b8ec8bf78eafddd87fca955ffb01d82c512b
Randy Carrero <randycarrero9@gmail.com>
2021-06-04 11:05:56 -0400

    updated to current settings

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,16 +1,13 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:gunpla_database/backend/backend.dart';
-import 'package:gunpla_database/home/gunpla_list_tile.dart';
-import 'package:gunpla_database/gunpla_details/gunpla_details_screen.dart';
-import 'package:gunpla_database/pages/profile.dart';
-import 'package:gunpla_database/pages/series.dart';
-import 'package:gunpla_database/pages/settings.dart';
-import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gunpla_database/authscreen/login_screen.dart';
+import 'package:gunpla_database/backend/auth_provider.dart';
+import 'package:gunpla_database/home/gunpla_list_page.dart';
 
 class HomeScreen extends StatefulWidget {
+  final CollectionReference collectionReference =
+      FirebaseFirestore.instance.collection('gunpla');
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -20,90 +17,59 @@ void _filterGunplas(value) {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int currentTAb = 0;
-  final List<Widget> screens = [HomeScreen(), Series(), Profile(), Setting()];
+  bool isSearching = false;
   List gunplas = [];
   List filteredgunplas = [];
-  bool isSearching = false;
-
-  final PageStorageBucket bucket = PageStorageBucket();
-  Widget currentScreen = HomeScreen();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: !isSearching
-            ? Text('Gunpla Database')
-            : TextField(
-                onChanged: (value) {
-                  _filterGunplas(value);
-                },
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                    icon: Icon(
-                      Icons.search,
-                      color: Colors.white,
-                    ),
-                    hintText: "Search Gunpla Here",
-                    hintStyle: TextStyle(color: Colors.white)),
-              ),
-        actions: <Widget>[
-          isSearching
-              ? IconButton(
-                  icon: Icon(Icons.cancel),
-                  onPressed: () {
-                    setState(() {
-                      this.isSearching = false;
-                    });
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              AuthClass().signOut();
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                  (route) => false);
+            },
+            icon: const Icon(Icons.logout),
+          ),
+          title: !isSearching
+              ? Text('Gunpla Database')
+              : TextField(
+                  onChanged: (value) {
+                    _filterGunplas(value);
                   },
-                )
-              : IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {
-                    setState(() {
-                      this.isSearching = true;
-                    });
-                  },
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      hintText: "Search Gunpla Here",
+                      hintStyle: TextStyle(color: Colors.white)),
                 ),
-        ],
-      ),
-      body: FutureBuilder(
-        future: context.read<Backend>().getGunplas(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(
-              child: Text('An error occurred.'),
-            );
-          } else if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            final gunplas = snapshot.data;
-
-            return ListView(
-              children: [
-                for (final gunpla in gunplas) ...[
-                  GunplaListTile(
-                    gunpla: gunpla,
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return GunplaDetailsScreen(gunpla: gunpla);
-                          },
-                        ),
-                      );
+          actions: <Widget>[
+            isSearching
+                ? IconButton(
+                    icon: Icon(Icons.cancel),
+                    onPressed: () {
+                      setState(() {
+                        this.isSearching = false;
+                      });
                     },
-                  ),
-                  const Divider(height: 0.0),
-                ]
-              ],
-            );
-          }
-        },
-      ),
-    );
+                  )
+                : IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () {
+                      setState(() {
+                        this.isSearching = true;
+                      });
+                    },
+                  )
+          ],
+        ),
+        body: GunplaListTile());
   }
 }
