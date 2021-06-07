c5a0de9b7135d5bea59413cc64f170faa43ba024
Randy Carrero <randycarrero9@gmail.com>
2021-06-06 20:30:41 -0400

    Removed  outdated code that will no longer be used

--- lib/backend/backend.dart
+++ lib/backend/backend.dart
@@ -1,16 +1,9 @@
 // import 'dart:convert';
 // import 'dart:io';
-
 // import 'package:meta/meta.dart';
 // import 'package:cloud_firestore/cloud_firestore.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:http/http.dart' as http;
 
-// List<Gunpla> gunplaFromJson(String str) =>
-//     List<Gunpla>.from(json.decode(str).map((x) => Gunpla.fromJson(x)));
-
-// String gunplaToJson(List<Gunpla> data) =>
-//     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 // class Backend {
 //   final String hostUrl;
@@ -50,73 +43,3 @@
 //         .doc(userId)
 //         .set({'favorited_gunplas': currentFavoritedGunplas});
 //   }
-
-//   Future<List<Gunpla>> getGunplas() async {
-//     final url = '$hostUrl';
-
-//     final response = await http.get(url);
-
-//     if (response.statusCode != 200) {
-//       throw HttpException(
-//         '${response.statusCode}: ${response.reasonPhrase}',
-//         uri: Uri.tryParse(url),
-//       );
-//     }
-
-//     final body = response.body;
-
-//     final jsonData = json.decode(body) as List;
-
-//     final gunplas =
-//         jsonData.map((jsonObject) => Gunpla.fromJson(jsonObject)).toList();
-
-//     return gunplas;
-//   }
-
-//   Future<void> signOut() async {
-//     await FirebaseAuth.instance.signOut();
-//   }
-
-//   Future<void> signUp() async {
-//     await FirebaseAuth.instance.signInAnonymously();
-//   }
-// }
-
-// class Gunpla {
-//   int id;
-
-//   String image;
-//   String name;
-//   String series;
-//   String grade;
-//   String scale;
-//   String exclusive;
-//   Gunpla({
-//     this.id,
-//     this.image,
-//     this.name,
-//     this.series,
-//     this.grade,
-//     this.scale,
-//     this.exclusive,
-//   });
-
-//   factory Gunpla.fromJson(Map<String, dynamic> json) => Gunpla(
-//       id: json["id"],
-//       image: json["image"] == null ? null : json["image"],
-//       name: json["name"],
-//       series: json["series"],
-//       grade: json["grade"],
-//       scale: json["scale"],
-//       exclusive: json["exclusive"]);
-
-//   Map<String, dynamic> toJson() => {
-//         "id": id,
-//         "image": image == null ? null : image,
-//         "name": name,
-//         "series": series,
-//         "grade": grade,
-//         "scale": scale,
-//         "exclusive": exclusive,
-//       };
-// }
