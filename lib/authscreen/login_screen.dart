3baed3ceec238bd092dc7f122112224555b310f6
Randy Carrero <randycarrero9@gmail.com>
2021-06-10 14:10:05 -0400

    Cleaned up UI

--- lib/authscreen/login_screen.dart
+++ lib/authscreen/login_screen.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:gunpla_database/authscreen/sign_up_screen.dart';
-import 'package:gunpla_database/backend/auth_provider.dart';
-import 'package:gunpla_database/home/home_screen.dart';
-import 'package:gunpla_database/app/constants.dart';
-
-import 'reset_screen.dart';
+import 'package:gunpla_database/app/widgets.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key key}) : super(key: key);
@@ -15,195 +10,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  TextEditingController _email = TextEditingController();
-  TextEditingController _password = TextEditingController();
-
   bool isLoading = false;
-
-  Widget _buildEmailTF() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          'Email',
-          style: kLabelStyle,
-        ),
-        SizedBox(height: 10.0),
-        Container(
-          alignment: Alignment.centerLeft,
-          decoration: kBoxDecorationStyle,
-          height: 60.0,
-          child: TextField(
-            keyboardType: TextInputType.emailAddress,
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'OpenSans',
-            ),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 14.0),
-              prefixIcon: Icon(
-                Icons.email,
-                color: Colors.white,
-              ),
-              hintText: 'Enter your Email',
-              hintStyle: kHintTextStyle,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildPasswordTF() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          'Password',
-          style: kLabelStyle,
-        ),
-        SizedBox(height: 10.0),
-        Container(
-          alignment: Alignment.centerLeft,
-          decoration: kBoxDecorationStyle,
-          height: 60.0,
-          child: TextField(
-            obscureText: true,
-            style: TextStyle(
-              color: Colors.white,
-              // fontFamily: 'OpenSans',
-            ),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 14.0),
-              prefixIcon: Icon(
-                Icons.lock,
-                color: Colors.white,
-              ),
-              hintText: 'Enter your Password',
-              hintStyle: kHintTextStyle,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildForgotPasswordBtn() {
-    return Container(
-      alignment: Alignment.centerRight,
-      child: TextButton(
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => ResetScreen()));
-        },
-        child: Text(
-          'Forgot Password?',
-          style: kLabelStyle,
-        ),
-      ),
-    );
-  }
-
-  final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
-    primary: Color(0xFF9E9E9E),
-    minimumSize: Size(88, 36),
-    elevation: 5.0,
-    padding: EdgeInsets.all(15.0),
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.all(Radius.circular(30.0)),
-    ),
-  );
-
-  Widget _buildLoginBtn() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 25.0),
-      width: double.infinity,
-      child: ElevatedButton(
-        style: raisedButtonStyle,
-        onPressed: () {
-          setState(() {
-            isLoading = true;
-          });
-          AuthClass()
-              .signIN(
-                  email: _email.text.trim(), password: _password.text.trim())
-              .then((value) {
-            if (value == "Welcome") {
-              setState(() {
-                isLoading = false;
-              });
-              Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomeScreen()),
-                  (route) => false);
-            } else {
-              setState(() {
-                isLoading = false;
-              });
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text(value)));
-            }
-          });
-        },
-        child: Text(
-          'LOGIN',
-          style: TextStyle(
-            color: Colors.white,
-            letterSpacing: 1.5,
-            fontSize: 18.0,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSignupBtn() {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => SignUpScreen()));
-      },
-      child: RichText(
-        text: TextSpan(
-          children: [
-            TextSpan(
-              text: 'Don\'t have an Account? ',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18.0,
-                // fontWeight: FontWeight.w400,
-              ),
-            ),
-            TextSpan(
-              text: 'Sign Up',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18.0,
-                // fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
-    // final ButtonStyle style = ElevatedButton.styleFrom(
-    //   textStyle: const TextStyle(
-    //     fontSize: 20,
-    //   ),
-    //   padding: EdgeInsets.symmetric(vertical: 15, horizontal: 50.0),
-    //   elevation: 10,
-    // );
     return Scaffold(
-        // appBar: AppBar(
-        //   title: Text("Login"),
-        // ),
         body: isLoading == false
             ? AnnotatedRegion<SystemUiOverlayStyle>(
                 value: SystemUiOverlayStyle.light,
@@ -249,14 +60,14 @@ class _LoginScreenState extends State<LoginScreen> {
                                 ),
                               ),
                               SizedBox(height: 30.0),
-                              _buildEmailTF(),
+                              BuildEmailTF(),
                               SizedBox(
                                 height: 30.0,
                               ),
-                              _buildPasswordTF(),
-                              _buildForgotPasswordBtn(),
-                              _buildLoginBtn(),
-                              _buildSignupBtn(),
+                              BuildPasswordTF(),
+                              BuildForgotPasswordBtn(),
+                              BuildLoginBtn(),
+                              BuildSignUpBtn(),
                             ],
                           ),
                         ),
