d374ab936b68d9fe6901225cd2e38cec687cdb34
Randy Carrero <randycarrero9@gmail.com>
2021-06-15 18:34:18 -0400

    Removed UI was not working.

--- lib/authscreen/login_screen.dart
+++ lib/authscreen/login_screen.dart
@@ -1,79 +1,136 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:gunpla_database/app/widgets.dart';
+import 'package:gunpla_database/authscreen/sign_up_screen.dart';
+import 'package:gunpla_database/backend/auth_provider.dart';
+import 'package:gunpla_database/home/home_screen.dart';
+
+import 'reset_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key key}) : super(key: key);
-
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _email = TextEditingController();
+  TextEditingController _password = TextEditingController();
+
   bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(
+        fontSize: 20,
+      ),
+      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 50.0),
+      elevation: 10,
+    );
     return Scaffold(
+        appBar: AppBar(
+          title: Text("Login"),
+        ),
         body: isLoading == false
-            ? AnnotatedRegion<SystemUiOverlayStyle>(
-                value: SystemUiOverlayStyle.light,
-                child: GestureDetector(
-                  onTap: () => FocusScope.of(context).unfocus(),
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        height: double.infinity,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              Color(0xFF616161),
-                              Color(0xFF424242),
-                              Color(0xFF303030),
-                              Color(0xFF212121),
-                            ],
-                            stops: [0.1, 0.4, 0.7, 0.9],
-                          ),
-                        ),
+            ? Padding(
+                padding: const EdgeInsets.all(48.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _email,
+                      keyboardType: TextInputType.emailAddress,
+                      autofocus: false,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.mail_outline),
+                        hintText: "Email",
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                        border: OutlineInputBorder(),
                       ),
-                      Container(
-                        height: double.infinity,
-                        child: SingleChildScrollView(
-                          physics: AlwaysScrollableScrollPhysics(),
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 40.0,
-                            vertical: 120.0,
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                'Sign In',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontFamily: 'OpenSans',
-                                  fontSize: 30.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(height: 30.0),
-                              BuildEmailTF(),
-                              SizedBox(
-                                height: 30.0,
-                              ),
-                              BuildPasswordTF(),
-                              BuildForgotPasswordBtn(),
-                              BuildLoginBtn(),
-                              BuildSignUpBtn(),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      controller: _password,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock_outlined),
+                        hintText: "Password",
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        AuthClass()
+                            .signIN(
+                                email: _email.text.trim(),
+                                password: _password.text.trim())
+                            .then((value) {
+                          if (value == "Welcome") {
+                            setState(() {
+                              isLoading = false;
+                            });
+                            Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomeScreen()),
+                                (route) => false);
+                          } else {
+                            setState(() {
+                              isLoading = false;
+                            });
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(SnackBar(content: Text(value)));
+                          }
+                        });
+                      },
+                      icon: Icon(Icons.login_outlined),
+                      label: Text("Log in "),
+                      style: style,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignUpScreen()));
+                        },
+                        child: Container(
+                          child: Text("Create New Account"),
+                          decoration: BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(
+                                      width: 1, color: Colors.black))),
+                        )),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ResetScreen()));
+                        },
+                        child: Container(
+                          child: Text("Forgot Password?"),
+                          decoration: BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(
+                                      width: 1, color: Colors.black))),
+                        ))
+                  ],
                 ),
               )
             : Center(
