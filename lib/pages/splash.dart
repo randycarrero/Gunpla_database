a383ba401ec020d8cbadb1956c7365b1f902553f
Randy Carrero <randycarrero9@gmail.com>
2021-06-15 20:30:51 -0400

    disable some unused imports

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gunpla_database/authscreen/login_screen.dart';
-import 'package:gunpla_database/authscreen/sign_up_screen.dart';
+// import 'package:gunpla_database/authscreen/sign_up_screen.dart';
 import 'package:gunpla_database/home/home_screen.dart';
 
 class Splash extends StatefulWidget {
@@ -16,7 +16,7 @@ class _SplashState extends State<Splash> {
   FirebaseAuth auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds:2), () {
+    Future.delayed(const Duration(seconds: 2), () {
       if (auth.currentUser == null) {
         Navigator.pushAndRemoveUntil(
             context,
@@ -29,7 +29,7 @@ class _SplashState extends State<Splash> {
             (route) => false);
       }
     });
-return Scaffold(
+    return Scaffold(
         body: Container(
             decoration: BoxDecoration(
                 image: DecorationImage(
