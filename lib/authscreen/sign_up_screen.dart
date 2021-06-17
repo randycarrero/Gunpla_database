84648e0199073a40e71e0a8344f2eb7a1a2c2245
Randy Carrero <randycarrero9@gmail.com>
2021-06-17 05:57:39 -0400

    Fully Functional UI implementation.

--- lib/authscreen/sign_up_screen.dart
+++ lib/authscreen/sign_up_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:gunpla_database/app/constants.dart';
 import 'package:gunpla_database/backend/auth_provider.dart';
 import 'package:gunpla_database/home/home_screen.dart';
+
 import 'login_screen.dart';
 
 class SignUpScreen extends StatefulWidget {
   SignUpScreen({Key key}) : super(key: key);
+
   @override
   _SignUpScreenState createState() => _SignUpScreenState();
 }
@@ -12,108 +16,199 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   TextEditingController _email = TextEditingController();
   TextEditingController _password = TextEditingController();
-
   bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    final ButtonStyle style = ElevatedButton.styleFrom(
-      textStyle: const TextStyle(
-        fontSize: 20,
-      ),
-      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 50.0),
-      elevation: 10,
-    );
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Register"),
-        ),
         body: isLoading == false
-            ? Padding(
-                padding: const EdgeInsets.all(48.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      controller: _email,
-                      keyboardType: TextInputType.emailAddress,
-                      autofocus: false,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.mail_outline),
-                        hintText: "Email",
-                        contentPadding:
-                            EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                        border: OutlineInputBorder(),
+            ? AnnotatedRegion<SystemUiOverlayStyle>(
+                value: SystemUiOverlayStyle.light,
+                child: GestureDetector(
+                  onTap: () => FocusScope.of(context).unfocus(),
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        height: double.infinity,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Color(0xFF616161),
+                              Color(0xFF424242),
+                              Color(0xFF303030),
+                              Color(0xFF212121),
+                            ],
+                            stops: [0.1, 0.4, 0.7, 0.9],
+                          ),
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    TextFormField(
-                      controller: _password,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.lock_outlined),
-                        hintText: "Password",
-                        contentPadding:
-                            EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 25,
-                    ),
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        setState(() {
-                          isLoading = true;
-                        });
-                        AuthClass()
-                            .createAccount(
-                                email: _email.text.trim(),
-                                password: _password.text.trim())
-                            .then((value) {
-                          if (value == "Account created") {
-                            setState(() {
-                              isLoading = false;
-                            });
-                            Navigator.pushAndRemoveUntil(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => HomeScreen()),
-                                (route) => false);
-                          } else {
-                            setState(() {
-                              isLoading = false;
-                            });
-                            ScaffoldMessenger.of(context)
-                                .showSnackBar(SnackBar(content: Text(value)));
-                          }
-                        });
-                      },
-                      icon: Icon(Icons.person_add_outlined),
-                      label: Text("Create account"),
-                      style: style,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => LoginScreen()));
-                        },
-                        child: Container(
-                          child: Text("Already have an account? Login"),
-                          decoration: BoxDecoration(
-                              border: Border(
-                                  bottom: BorderSide(
-                                      width: 1, color: Colors.black))),
-                        )),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                  ],
+                      Container(
+                        height: double.infinity,
+                        child: SingleChildScrollView(
+                          physics: AlwaysScrollableScrollPhysics(),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 40.0,
+                            vertical: 120.0,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                'Register',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'OpenSans',
+                                  fontSize: 30.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 30.0),
+                              Text(
+                                'Email',
+                                style: kLabelStyle,
+                              ),
+                              SizedBox(height: 10.0),
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                decoration: kBoxDecorationStyle,
+                                height: 60.0,
+                                child: TextField(
+                                  controller: _email,
+                                  keyboardType: TextInputType.emailAddress,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'OpenSans',
+                                  ),
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    contentPadding: EdgeInsets.only(top: 14.0),
+                                    prefixIcon: Icon(
+                                      Icons.email,
+                                      color: Colors.white,
+                                    ),
+                                    hintText: 'Enter your Email',
+                                    hintStyle: kHintTextStyle,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 30.0,
+                              ),
+                              Text(
+                                'Password',
+                                style: kLabelStyle,
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                decoration: kBoxDecorationStyle,
+                                height: 60.0,
+                                child: TextField(
+                                  controller: _password,
+                                  obscureText: true,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    contentPadding: EdgeInsets.only(top: 14.0),
+                                    prefixIcon: Icon(
+                                      Icons.lock,
+                                      color: Colors.white,
+                                    ),
+                                    hintText: 'Enter your Password',
+                                    hintStyle: kHintTextStyle,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.symmetric(vertical: 25.0),
+                                width: double.infinity,
+                                child: ElevatedButton(
+                                  style: raisedButtonStyle,
+                                  onPressed: () {
+                                    setState(() {
+                                      isLoading = true;
+                                    });
+                                    AuthClass()
+                                        .createAccount(
+                                            email: _email.text.trim(),
+                                            password: _password.text.trim())
+                                        .then((value) {
+                                      if (value == "Account created") {
+                                        setState(() {
+                                          isLoading = false;
+                                        });
+                                        Navigator.pushAndRemoveUntil(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    HomeScreen()),
+                                            (route) => false);
+                                      } else {
+                                        setState(() {
+                                          isLoading = false;
+                                        });
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(
+                                                SnackBar(content: Text(value)));
+                                      }
+                                    });
+                                  },
+                                  child: Text(
+                                    'REGISTER',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      letterSpacing: 1.5,
+                                      fontSize: 18.0,
+                                      fontFamily: 'OpenSans',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => LoginScreen()));
+                                },
+                                child: RichText(
+                                  text: TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: 'Already have an account? ',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: 'Login',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18.0,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               )
             : Center(
