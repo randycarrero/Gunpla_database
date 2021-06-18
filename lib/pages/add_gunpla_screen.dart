56d200606bca9ef859f2e70a3c9aa1b7b0cf8351
Randy Carrero <randycarrero9@gmail.com>
2021-06-18 14:53:25 -0400

    Firestore integration
    now it will show up in firestore when data is added.

--- lib/pages/add_gunpla_screen.dart
+++ lib/pages/add_gunpla_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gunpla_database/app/constants.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AddGunplaScreen extends StatefulWidget {
   AddGunplaScreen({Key key}) : super(key: key);
@@ -16,6 +16,24 @@ class _AddGunplaScreenState extends State<AddGunplaScreen> {
   TextEditingController scaleController = TextEditingController();
   TextEditingController exclusiveController = TextEditingController();
   TextEditingController imageUrlController = TextEditingController();
+
+  Map<String, dynamic> gunplaToAdd;
+  CollectionReference collectionReference =
+      FirebaseFirestore.instance.collection('gunpla');
+
+  addGunpla() {
+    gunplaToAdd = {
+      "name": nameController.text,
+      "series": seriesController.text,
+      "grade": gradeController.text,
+      "scale": scaleController.text,
+      "exclusive": exclusiveController.text,
+      "image": imageUrlController.text,
+    };
+    collectionReference
+        .add(gunplaToAdd)
+        .whenComplete(() => print("added to Database"));
+  }
 
   _buildTextField(TextEditingController controller, String labelText) {
     return Container(
@@ -40,15 +58,61 @@ class _AddGunplaScreenState extends State<AddGunplaScreen> {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          _buildTextField(nameController, "Name"),
-          _buildTextField(seriesController, "Series"),
-          _buildTextField(gradeController, "Grade"),
-          _buildTextField(scaleController, "Scale"),
-          _buildTextField(exclusiveController, "Exclusive"),
-          _buildTextField(imageUrlController, "Image Url"),
-        ],
+      backgroundColor: Colors.grey.shade900,
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Text(
+              "Add Gunpla",
+              style: TextStyle(color: Colors.white, fontSize: 28),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            _buildTextField(nameController, "Name"),
+            SizedBox(
+              height: 20,
+            ),
+            _buildTextField(seriesController, "Series"),
+            SizedBox(
+              height: 20,
+            ),
+            _buildTextField(gradeController, "Grade"),
+            SizedBox(
+              height: 20,
+            ),
+            _buildTextField(scaleController, "Scale"),
+            SizedBox(
+              height: 20,
+            ),
+            _buildTextField(exclusiveController, "Exclusive"),
+            SizedBox(
+              height: 20,
+            ),
+            _buildTextField(imageUrlController, "Image Url"),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: raisedButtonStyle,
+                onPressed: () {},
+                child: Text(
+                  'Add Gunpla to Database',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 1.5,
+                    fontSize: 12.0,
+                    fontFamily: 'OpenSans',
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
