7f1a26998d326db9cd29ff0adbc3f38e121c2c8f
Randy Carrero <randycarrero9@gmail.com>
2021-07-01 14:29:57 -0400

    Switch Button implemented

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -6,12 +6,36 @@ class Setting extends StatefulWidget {
 }
 
 class _SettingState extends State<Setting> {
+  bool _flutter = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Setting')),
-      body:
-          Center(child: Text('Setting Screen', style: TextStyle(fontSize: 40))),
+      body: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Card(
+              child: SwitchListTile(
+                title: Text(
+                  'Light Mode?',
+                  style: TextStyle(fontSize: 20),
+                ),
+                value: _flutter,
+                activeColor: Colors.amber[700],
+                inactiveTrackColor: Colors.grey,
+                onChanged: (bool value) {
+                  setState(() {
+                    _flutter = value;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.trailing,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
