ca05c72e430e7c95044f36ceb5b20f0c3bea36b1
Randy Carrero <randycarrero9@gmail.com>
2021-07-05 06:53:29 -0400

    Update backend.dart

--- lib/backend/backend.dart
+++ lib/backend/backend.dart
@@ -1,45 +1,39 @@
-// import 'dart:convert';
-// import 'dart:io';
-// import 'package:meta/meta.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:meta/meta.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+class Backend {
+  Stream<User> get currentUserStream => FirebaseAuth.instance.userChanges();
 
-// class Backend {
-//   final String hostUrl;
+  Stream<List<String>> get favoritedGunplas {
+    final userId = FirebaseAuth.instance.currentUser.uid;
+    return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .snapshots()
+        .map((documentSnapshot) {
+      if (!documentSnapshot.exists) {
+        return [];
+      }
+      return List<String>.from(documentSnapshot.data()['favorited_gunplas']);
+    });
+  }
 
-//   const Backend(this.hostUrl);
+  Future<void> setFavoritedGunpla({
+    @required String id,
+    @required bool favorited,
+  }) async {
+    final currentFavoritedGunplas = await favoritedGunplas.first;
 
-//   Stream<User> get currentUserStream => FirebaseAuth.instance.userChanges();
-
-//   Stream<List<String>> get favoritedGunplas {
-//     final userId = FirebaseAuth.instance.currentUser.uid;
-//     return FirebaseFirestore.instance
-//         .collection('users')
-//         .doc(userId)
-//         .snapshots()
-//         .map((documentSnapshot) {
-//       if (!documentSnapshot.exists) {
-//         return [];
-//       }
-//       return List<String>.from(documentSnapshot.data()['favorited_gunplas']);
-//     });
-//   }
-
-//   Future<void> setFavoritedGunpla({
-//     @required String id,
-//     @required bool favorited,
-//   }) async {
-//     final currentFavoritedGunplas = await favoritedGunplas.first;
-
-//     if (favorited && !currentFavoritedGunplas.contains(id)) {
-//       currentFavoritedGunplas.add(id);
-//     } else if (!favorited && currentFavoritedGunplas.contains(id)) {
-//       currentFavoritedGunplas.remove(id);
-//     }
-//     final userId = FirebaseAuth.instance.currentUser.uid;
-//     await FirebaseFirestore.instance
-//         .collection('users')
-//         .doc(userId)
-//         .set({'favorited_gunplas': currentFavoritedGunplas});
-//   }
+    if (favorited && !currentFavoritedGunplas.contains(id)) {
+      currentFavoritedGunplas.add(id);
+    } else if (!favorited && currentFavoritedGunplas.contains(id)) {
+      currentFavoritedGunplas.remove(id);
+    }
+    final userId = FirebaseAuth.instance.currentUser.uid;
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .set({'favorited_gunplas': currentFavoritedGunplas});
+  }
+}
