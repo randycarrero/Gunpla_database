b27211862bb7746a7b0aa6c0e3f37e504b5430ca
Randy Carrero <randycarrero9@gmail.com>
2021-07-22 16:12:11 -0400

    Rating widgets added
    Will need to change the normal status and find firebase integration to pull data from.

--- lib/gunpla_details/gunpla_details_screen.dart
+++ lib/gunpla_details/gunpla_details_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ant_icons/ant_icons.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:gunpla_database/app/constants.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class LeadingImage extends StatelessWidget {
   final String url;
@@ -23,13 +24,25 @@ class LeadingImage extends StatelessWidget {
   }
 }
 
-class GunplaDetailsScreen extends StatelessWidget {
-  final CollectionReference collectionReference =
-      FirebaseFirestore.instance.collection('gunpla');
-
+class GunplaDetailsScreen extends StatefulWidget {
   GunplaDetailsScreen(this.document);
 
   final document;
+
+  @override
+  _GunplaDetailsScreenState createState() => _GunplaDetailsScreenState();
+}
+
+class _GunplaDetailsScreenState extends State<GunplaDetailsScreen> {
+  final CollectionReference collectionReference =
+      FirebaseFirestore.instance.collection('gunpla');
+  double _rating;
+
+  @override
+  void initState() {
+    super.initState();
+    _rating = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,38 +68,67 @@ class GunplaDetailsScreen extends StatelessWidget {
       body: ListView(
         children: [
           LeadingImage(
-            document['image'],
+            widget.document['image'],
           ),
           ListTile(
             title: Text(
-              document['name'],
+              widget.document['name'],
               style: textTheme.headline6,
             ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: Text(
-              document['series'],
+              widget.document['series'],
               style: textTheme.subtitle1,
             ),
           ),
           const Divider(),
           ListTile(
             leading: const Icon(AntIcons.column_width),
-            title: Text(document['scale']),
+            title: Text(widget.document['scale']),
             subtitle: const Text('Available Scale(s)'),
           ),
           const Divider(),
           ListTile(
             leading: const Icon(AntIcons.colum_height),
-            title: Text(document['grade']),
+            title: Text(widget.document['grade']),
             subtitle: const Text('Available Grade'),
           ),
           const Divider(),
           ListTile(
             leading: const Icon(Icons.star),
-            title: Text(document['exclusive']),
+            title: Text(widget.document['exclusive']),
             subtitle: const Text('Exclusive'),
+          ),
+          const Divider(),
+          // RatingBar.builder()
+          Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                  child: RatingBar.builder(
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    itemCount: 5,
+                    itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                    itemBuilder: (context, _) => Icon(
+                      Icons.star,
+                      color: Colors.amber[700],
+                    ),
+                    onRatingUpdate: (double rating) {
+                      setState(() {
+                        _rating = rating;
+                      });
+                    },
+                  ),
+                ),
+                Text('Rating: $_rating',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
           ),
           const Divider(),
           Padding(
