c83e3b437befe055a7f751b80f8117c8003a411a
Gunpla Database admin <randycarrero9@gmail.com>
2021-07-26 16:26:22 -0400

    details Changed

--- lib/gunpla_details/gunpla_details_screen.dart
+++ lib/gunpla_details/gunpla_details_screen.dart
@@ -23,13 +23,19 @@ class LeadingImage extends StatelessWidget {
   }
 }
 
-class GunplaDetailsScreen extends StatelessWidget {
-  final CollectionReference collectionReference =
-      FirebaseFirestore.instance.collection('gunpla');
-
+class GunplaDetailsScreen extends StatefulWidget {
   GunplaDetailsScreen(this.document);
 
   final document;
+
+  @override
+  _GunplaDetailsScreenState createState() => _GunplaDetailsScreenState();
+}
+
+class _GunplaDetailsScreenState extends State<GunplaDetailsScreen> {
+  final CollectionReference collectionReference =
+      FirebaseFirestore.instance.collection('gunpla');
+  Color _iconColor = Colors.grey;
 
   @override
   Widget build(BuildContext context) {
@@ -40,68 +46,72 @@ class GunplaDetailsScreen extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
         actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.favorite_border,
-            ),
-            // ),
-            // IconButton(
-            //   onPressed: () {},
-            //   icon: Icon(Icons.edit),
-          )
+          StreamBuilder<Object>(
+              stream: null,
+              builder: (context, snapshot) {
+                return IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _iconColor = Colors.redAccent;
+                    });
+                  },
+                  icon: Icon(Icons.favorite, color: _iconColor),
+                  // ),
+                  // IconButton(
+                  //   onPressed: () {},
+                  //   icon: Icon(Icons.edit),
+                );
+              })
         ],
       ),
       body: ListView(
         children: [
           LeadingImage(
-            document['image'],
+            widget.document['image'],
           ),
           ListTile(
             title: Text(
-              document['name'],
+              widget.document['name'],
               style: textTheme.headline6,
             ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: Text(
-              document['series'],
+              widget.document['series'],
               style: textTheme.subtitle1,
             ),
           ),
           const Divider(),
           ListTile(
             leading: const Icon(AntIcons.column_width),
-            title: Text(document['scale']),
+            title: Text(widget.document['scale']),
             subtitle: const Text('Available Scale(s)'),
           ),
           const Divider(),
           ListTile(
             leading: const Icon(AntIcons.colum_height),
-            title: Text(document['grade']),
+            title: Text(widget.document['grade']),
             subtitle: const Text('Available Grade'),
           ),
           const Divider(),
           ListTile(
             leading: const Icon(Icons.star),
-            title: Text(document['exclusive']),
+            title: Text(widget.document['exclusive']),
             subtitle: const Text('Exclusive'),
           ),
           const Divider(),
           Padding(
-            padding: EdgeInsets.fromLTRB(0, 12, 0, 24),
-            child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    style: raisedButtonStyle,
-                    onPressed: () {},
-                    child: Text("Gunpla Built"),
-                  )
-                ]),
+            padding: const EdgeInsets.all(10.0),
+            child: FloatingActionButton.extended(
+              backgroundColor: const Color(0xffffBF00),
+              foregroundColor: Colors.black,
+              onPressed: () {
+                // Respond to button press
+              },
+              icon: Icon(Icons.add),
+              label: Text('Gunpla Built'),
+            ),
           ),
         ],
       ),
